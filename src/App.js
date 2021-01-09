80f2b37321563c0f299b8ced24567edeca9427e2
mogobanyamwaro <mogobanyamwaro@gmail.com>
2021-01-09 15:06:45 +0300

    updated the version using the router

--- src/App.js
+++ src/App.js
@@ -3,15 +3,28 @@ import './App.css';
 import Nav from './Nav'
 import About from './About'
 import Shop from'./Shop'
+import {BrowserRouter as Router, Switch,Route} from  'react-router-dom'
 function App() {
   return (
-    <div className="App">
+   <Router>
+      <div className="App">
       <Nav/>
-      <About/>
-      <Shop/>
+      <Switch>
+      <Route path= "/" exact components = {Home}/>
+     <Route path= "/shop" components = {About}/>
+     <Route path= "/shop" components = {Shop}/>
+      </Switch>
+
+      
 
     </div>
+   </Router>
   );
+}
+const Home =()=>{
+  <div>
+    <h1>Home Page</h1>
+  </div>
 }
 
 export default App;
