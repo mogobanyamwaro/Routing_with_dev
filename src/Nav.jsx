3289995d4656795e147e5ee01d34621c11a370b1
mogobanyamwaro <mogobanyamwaro@gmail.com>
2021-01-09 14:42:54 +0300

    started using routing

---
+++ src/Nav.jsx
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+function Nav() {
+  return (
+    <div className="App">
+        <nav>
+            <h1>Logo</h1>
+            <ul className = "nav-links">
+                <li>About</li>
+                <li>shop</li>
+            </ul>
+        </nav>
+
+    </div>
+  );
+}
+
+export default Nav;
---
+++ src/Shop.jsx
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function Shop() {
+  return (
+    <div className="App">
+<h1>shop page </h1>
+
+    </div>
+  );
+}
+
+export default Shop;
