d51d496644cacf7a6a79153e84deaa358e9c9835
mogobanyamwaro <mogobanyamwaro@gmail.com>
2021-01-09 15:23:05 +0300

    used the links

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,14 +1,22 @@
 
 import './App.css';
-
+import {Link} from 'react-router-dom'
 function Nav() {
+  const navstyle ={
+    color: 'white'
+  }
   return (
     <div className="App">
         <nav>
             <h1>Logo</h1>
             <ul className = "nav-links">
-                <li>About</li>
-                <li>shop</li>
+              <Link to = "/about" style = {navstyle}>
+              <li>About</li>
+              </Link>
+                
+              <Link to ="shop" style = {navstyle}>
+              <li>shop</li>
+              </Link>
             </ul>
         </nav>
 
